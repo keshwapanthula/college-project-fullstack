9355d1320e724e09a5d53628616418bfd25d78d4
keshwapanthula <keshwapanthula@oakland.edu>
2026-03-03 00:07:27 -0500

    fix: Add missing Profile import to CloudIntegrationController

--- CollegeAdmin/src/main/java/com/admin/controller/CloudIntegrationController.java
+++ CollegeAdmin/src/main/java/com/admin/controller/CloudIntegrationController.java
@@ -13,6 +13,7 @@ import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
+import org.springframework.context.annotation.Profile;
 
 import java.io.InputStream;
 import java.util.HashMap;
